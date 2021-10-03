d1526737ae90f9c9c14367f98f46ddeba268d6ed
sum16 <godasper@gmail.com>
2021-10-03 16:20:45 +0900

    フィールドを作成 #3

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-export const Field = () => {
-  return(
-  <div className="filed">
-    <p>フィールド</p>
-  </div>
-
-  ) 
+export const Field = ({ fields }) => {
+  return (
+    <div className="field">
+      {
+        fields.map((row) => {
+          return row.map((column) => {
+            return <div className={`dots ${column}`}></div>
+          })
+        })
+      }
+    </div>
+  )
 }
+
+// Array.fill()は、配列の全要素に同じ値を設定する関数です。 以下の例では、3個の数値配列を生成して値 0 で初期化しています。
+
+//const arr = Array(3).fill(0)
