77e7f44f9f3b8f33414c8bc2918d597c509598a7
sum16 <godasper@gmail.com>
2021-10-05 19:51:08 +0900

    一時停止処理の実装

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-export const Button = ({ status, onStart, onRestart}) => {
+export const Button = ({ status, onStart, onRestart, onStop}) => {
   return (
     <div className="button">
-      {
-        status === "gameover" ?
-        <button onClick={onRestart}>gameover</button>
-        :
-        <button onClick={onStart}>start</button>
-      }
+      {status === 'gameover' &&  <button onClick={onRestart}>gameover</button>}
+      {status === 'init' &&  <button onClick={onStart}>start</button>}
+      {status === 'suspended' &&  <button onClick={onStart}>start</button>}
+      {status === 'playing' &&  <button onClick={onStop}>stop</button>}
   </div>
   )
 }
